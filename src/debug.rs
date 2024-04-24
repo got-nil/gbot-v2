33d2bb3d9a8cb8d8f06ad4131a50dbcbe5c996ec
MorgVerd <morgverd@gmail.com>
2024-04-24 20:12:45 +0100

    feat: Debug functions

--- src/debug.rs
+++ src/debug.rs
@@ -1,15 +1,25 @@
-use rglua::lua::LuaState;
+use rglua::interface;
 use rglua::prelude::*;
+use crate::lua::fns::safe_log;
 
 #[lua_function]
-fn run_queue(l: LuaState) -> Result<i32, rglua::interface::Error> {
-    crate::run_queue(Some(l));
+fn run_queue(l: LuaState) -> Result<i32, interface::Error> {
+
+    let state = Some(l);
+
+    safe_log(state, "PRE RUNNING QUEUE");
+    crate::run_queue(state);
+    safe_log(state, "POST RUNNING QUEUE");
+
     Ok(0)
 }
 
 pub fn apply(l: LuaState) -> () {
-    let lib = reg! [
-        "run_queue" => run_queue
+
+    printgm!(l, "Applying debug functions! These leave a trace in the menu state!");
+
+    let lib = reg![
+        "RunQueue" => run_queue
     ];
     luaL_register(l, cstr!("gtest"), lib.as_ptr());
 }
